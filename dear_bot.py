448accc557983152838518ef5bea0d3982ae377b
Rahul Reddipalli <rahul.reddipalli@gmail.com>
2022-05-21 17:18:29 +0100

    Commented elements for deployment testing

--- dear_bot.py
+++ dear_bot.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
-from chatbot import Chatbot
+#from chatbot import Chatbot
 
 if __name__ == '__main__' :
     app = Flask(__name__)
-    bot = Chatbot("DearBot")
+    # bot = Chatbot("DearBot")
 
     @app.post('/get_response')
     def get_response():
         data = request.json
         user_input = request.form['chat']
-        predictions=bot.get_response(user_input)
+        predictions="t" #bot.get_response(user_input)
 
         return str(predictions)
-
 
     app.run(debug=True)
