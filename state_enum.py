35ee26937435bae4522cd2114fc01a5b8a4316d9
Rahul Reddipalli <rahul.reddipalli@gmail.com>
2022-05-22 23:19:24 +0100

    added functioanlity to add user to csv

--- state_enum.py
+++ state_enum.py
@@ -1,8 +1,19 @@
 from enum import Enum, auto
 
+
 class STATE(Enum):
     GREETING = auto()
-    RUNNING=auto()
-    ASKED_NAME=auto()
-    ADD_ENTRY =auto()
-    QUIT=-1
+    RUNNING = auto()
+
+    CREATE_PROFILE_NAME = auto()
+    CONFIRM_NAME = auto()
+
+    CREATE_PROFILE_PHRASE = auto()
+    CONFIRM_PHRASE = auto()
+
+    CHECK_IF_NEW = auto()
+
+    ADD_ENTRY = auto()
+    VIEW_ENTRY = auto()
+
+    QUIT = -1
