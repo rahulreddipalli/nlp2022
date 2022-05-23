6f8fd4ecf3eaa1ae315a17580cbcc1fd9a4d9684
Grace <gl00233@surrey.ac.uk>
2022-05-23 18:09:47 +0100

    created intent handler

--- intent_handler.py
+++ intent_handler.py
@@ -1,14 +1,46 @@
-import random as rand
-intents = ["greeting","goodbye","view_entry","add_entry"]
-responses = {i:[] for i in intents}
+import chatbot_logger
 
-responses["greeting"].append("Hi {} how are you?")
-responses["greeting"].append("Hola {}")
+import torch
+import string
 
-responses["goodbye"].append("Goodbye {}")
-responses["goodbye"].append("See ya later {}")
-def get_intent(user_input):
-    return intents[3]
+import numpy as np
 
-def get_response_by_intent(intent):
-    return rand.choice(responses[intent])
+import tensorflow as tf
+import pickle
+
+from keras.models import load_model
+from keras.layers import TextVectorization
+
+cuda_available = torch.cuda.is_available()
+
+model = load_model("intent classifier/intents.h5")
+
+from_disk = pickle.load(open("intent classifier/vectoriser.pkl", "rb"))
+
+vectoriser = TextVectorization.from_config(from_disk['config'])
+vectoriser.adapt(tf.data.Dataset.from_tensor_slices(["xyz"]))  # call adapt on dummy data
+vectoriser.set_weights(from_disk['weights'])
+encoder = pickle.load(open("intent classifier/encoder.pkl", "rb"))
+
+class_names = encoder.classes_
+
+input_str = tf.keras.Input(shape=(1,), dtype="string")
+x = vectoriser(input_str)  # vectorize
+output = model(x)
+intent_classifier = tf.keras.Model(input_str, output)
+
+
+def clean(message):
+    message = " ".join([word.lower() for word in message.split()])
+
+    remove = str.maketrans((string.punctuation + '£' + string.digits), ' '*len((string.punctuation + '£' + string.digits)))
+    result = message.translate(remove)
+    return result
+
+
+def predict_intent(user_input):
+    user_input = clean(user_input)
+    prediction = intent_classifier.predict([[user_input]])
+    intent = class_names[np.argmax(prediction[0])]
+    chatbot_logger.log_prediction("Predicting user intent", user_input, intent)
+    return intent
